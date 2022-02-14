2391c8bc2424ea6818539fc580b98fab6b81257d
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-02-14 23:48:52 +0000

    cognito-lambda-authorizer

---
+++ apigateway-cognito-lambda-authorizer/README.md
@@ -0,0 +1,40 @@
+# Run Terraform
+
+```
+terraform init; terraform plan; terraform apply --auto-approve
+```
+
+# Create Cognito users
+
+Using the outputs run the following commands,
+
+```
+ awslocal cognito-idp sign-up \                                                                                                       0 (9.199s) < 23:44:25
+      --client-id <user_pool_client_id> \
+      --username "user@domain.com" \
+      --password "password"
+```
+
+then,
+
+```
+awslocal cognito-idp admin-confirm-sign-up \                                                                                         0 (1.442s) < 23:46:06
+      --user-pool-id <user_pool_id> \
+      --username "user@domain.com"
+```
+
+then,
+
+```
+awslocal cognito-idp initiate-auth \                                                                                                 0 (0.601s) < 23:46:07
+      --auth-flow USER_PASSWORD_AUTH \
+      --auth-parameters USERNAME="user@domain.com",PASSWORD="password" \
+      --client-id <user_pool_client_id>
+```
+
+
+then,
+
+```
+curl -vvv "https://<rest_api_id>.execute-api.localhost.localstack.cloud:4566/local/demo" --header "Authorization: bearer <id-token>"
+```
---
+++ apigateway-cognito-lambda-authorizer/main.tf
@@ -0,0 +1,106 @@
+resource "aws_api_gateway_rest_api" "demo" {
+  name = "auth-demo"
+}
+
+resource "aws_api_gateway_resource" "demo" {
+  rest_api_id = aws_api_gateway_rest_api.demo.id
+  parent_id   = aws_api_gateway_rest_api.demo.root_resource_id
+  path_part   = "demo"
+}
+
+resource "aws_api_gateway_method" "any" {
+  rest_api_id   = aws_api_gateway_rest_api.demo.id
+  resource_id   = aws_api_gateway_resource.demo.id
+  http_method   = "ANY"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.demo.id
+}
+
+resource "aws_api_gateway_authorizer" "demo" {
+  name            = "demo"
+  type            = "COGNITO_USER_POOLS"
+  rest_api_id     = aws_api_gateway_rest_api.demo.id
+  provider_arns   = [aws_cognito_user_pool.pool.arn]
+  identity_source = "method.request.header.Authorization"
+}
+
+#
+# Integration
+#
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.demo.id
+  resource_id             = aws_api_gateway_resource.demo.id
+  http_method             = aws_api_gateway_method.any.http_method
+  integration_http_method = "ANY"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "lambda.zip"
+  function_name = "mylambda"
+  role          = aws_iam_role.role.arn
+  handler       = "lambda.handler"
+
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name = "myrole"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+
+#
+# Cognito
+#
+resource "aws_cognito_user_pool" "pool" {
+  name = "user_pool"
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name         = "example_external_api"
+  user_pool_id = aws_cognito_user_pool.pool.id
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_USER_SRP_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
+}
+
+output "rest_api_id" {
+	value = aws_api_gateway_rest_api.demo.id
+}
+
+output "user_pool_client_id" {
+	value = aws_cognito_user_pool_client.client.id
+}
+
+output "user_pool_id" {
+	value = aws_cognito_user_pool.pool.id
+}
