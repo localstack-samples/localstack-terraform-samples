44fe719dc4702cd2a9574cda870dbd992a9c7436
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-02-17 13:04:24 +0000

    apigateway SNS integration

---
+++ apigateway-sns-deploy/main.tf
@@ -0,0 +1,100 @@
+# https://docs.aws.amazon.com/sns/latest/api/API_Publish.html#API_Publish_Examples
+
+data "aws_region" "current" {}
+
+resource "aws_api_gateway_rest_api" "rest" {
+  name = "sns ingest"
+}
+
+resource "aws_api_gateway_resource" "ingest" {
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+  parent_id   = aws_api_gateway_rest_api.rest.root_resource_id
+  path_part   = "ingest"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest.id
+  resource_id   = aws_api_gateway_resource.ingest.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest.id
+  resource_id             = aws_api_gateway_resource.ingest.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+	credentials             = aws_iam_role.sns_publish.arn
+	uri = "arn:aws:apigateway:${data.aws_region.current.name}:sns:path//"
+
+	request_parameters = {
+    "integration.request.header.Content-Type" = "'application/x-www-form-urlencoded'"
+  }
+
+  request_templates       = {
+    "application/json" = "Action=Publish&TopicArn=$util.urlEncode('${aws_sns_topic.topic.arn}')&Message=$util.urlEncode($input.body)"
+  }
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+  resource_id = aws_api_gateway_resource.ingest.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+  resource_id = aws_api_gateway_resource.ingest.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+
+  # Transforms the backend JSON response to XML
+  response_templates = {
+    "application/json" = <<EOF
+{"status": "message published"}
+EOF
+  }
+}
+
+resource "aws_sns_topic" "topic" {
+  name = "event-topic"
+}
+
+
+resource "aws_iam_role" "sns_publish" {
+  name = "sns-publish-role"
+  assume_role_policy = data.aws_iam_policy_document.apigw.json
+}
+
+data "aws_iam_policy_document" "apigw" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "apigateway.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "sns_publish" {
+  name = "SNS-Publish"
+  role = aws_iam_role.sns_publish.id
+  policy = data.aws_iam_policy_document.sns_publish.json
+}
+
+data "aws_iam_policy_document" "sns_publish" {
+  statement {
+     actions = [
+      "sns:Publish",
+    ]
+
+    resources = [
+			aws_sns_topic.topic.arn
+    ]
+  }
+}
