39a64fe74dc793a0166e4b75aa95771d190f14f3
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-02-19 13:51:43 +0000

    runnable example

---
+++ apigateway-lambda-proxy-handling-binary/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+terraform init; terraform plan; terraform apply --auto-approve
+
+restapi=$(aws --endpoint-url=http://localhost:4566 apigateway get-rest-apis | jq -r .items[0].id)
+curl -X POST "$restapi.execute-api.localhost.localstack.cloud:4566/local/test" -H 'content-type: application/json' -d '{ "greeter": "cesar" }'
+curl -X POST "$restapi.execute-api.localhost.localstack.cloud:4566/local/test?demo64Flag=1" 'content-type: image/jpeg' --data-binary @./nyan-cat.jpg --output -
