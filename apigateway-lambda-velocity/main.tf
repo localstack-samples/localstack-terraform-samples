bfae51cc1fc5dc3ce6a318cb6174e5b5e54a6c5c
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-02-28 17:52:48 +0000

    add lambda velocity

---
+++ apigateway-lambda-velocity/main.tf
@@ -0,0 +1,97 @@
+# https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+# curl -X POST "mtnpa8am4z.execute-api.localhost.localstack.cloud:4566/local/test" -H 'content-type: application/json' -d '{ "greeter": "jane" }'
+# curl -X POST "mtnpa8am4z.execute-api.localhost.localstack.cloud:4566/local/test" -H 'greeter: jane'
+resource "aws_api_gateway_rest_api" "rest" {
+  name = "HelloWorld (AWS CLI)"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.rest.id
+  parent_id   = aws_api_gateway_rest_api.rest.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "ANY"
+  type                    = "AWS"
+	uri                     = aws_lambda_function.lambda.invoke_arn
+
+
+  request_templates = {
+    "application/json" = <<EOF
+#set( $body = $input.json("$") )
+
+#macro( loop $map )
+{
+    #foreach($key in $map.keySet())
+        #set( $k = $util.escapeJavaScript($key) )
+        #set( $v = $util.escapeJavaScript($map.get($key)).replaceAll("\\'", "'") )
+        "$k": "$v"
+        #if( $foreach.hasNext ) , #end
+    #end
+}
+#end
+
+{
+    "enhancedAuthContext": #loop($context.authorizer),
+
+    "headers": #loop($input.params().header),
+
+    "query": #loop($input.params().querystring),
+
+    "path": #loop($input.params().path),
+
+    "identity": #loop($context.identity),
+
+    "stageVariables": #loop($stageVariables),
+}
+EOF
+}
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "lambda.zip"
+  function_name = "mylambda"
+  role          = aws_iam_role.role.arn
+  handler       = "lambda.handler"
+
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name = "myrole"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
