d701c6673308b4fb1c4f807769efbde2dffd2847
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-04-05 09:05:09 +0100

    apigatewayv2-http-proxy-authorizer

---
+++ apigatewayv2-http-proxy-authorizer/main.tf
@@ -0,0 +1,67 @@
+# curl -X POST "a750e94d.execute-api.localhost.localstack.cloud:4566/example/test" -H 'content-type: application/json' -d '{ "greeter": "cesar" }'
+
+resource "aws_apigatewayv2_api" "example" {
+  name          = "example-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_authorizer" "example" {
+  api_id           = aws_apigatewayv2_api.example.id
+  authorizer_type  = "REQUEST"
+  authorizer_uri   = aws_lambda_function.authorizer.invoke_arn
+  identity_sources = ["$request.header.Cookie"]
+  name             = "authorizer"
+}
+
+resource "aws_lambda_function" "authorizer" {
+  filename      = "authorizer.zip"
+  function_name = "authorizer"
+  role          = aws_iam_role.role.arn
+  handler       = "authorizer.handler"
+  runtime       = "nodejs12.x"
+}
+
+resource "aws_iam_role" "role" {
+  name = "myrole"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_apigatewayv2_route" "example" {
+  api_id             = aws_apigatewayv2_api.example.id
+  route_key          = "ANY /{proxy+}"
+  authorization_type = "CUSTOM"
+  authorizer_id      = aws_apigatewayv2_authorizer.example.id
+  target             = "integrations/${aws_apigatewayv2_integration.example.id}"
+}
+
+resource "aws_apigatewayv2_integration" "example" {
+  api_id                 = aws_apigatewayv2_api.example.id
+  integration_type       = "AWS_PROXY"
+  payload_format_version = "2.0"
+  description            = "Lambda example"
+  integration_method     = "ANY"
+  integration_uri        = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "lambda.zip"
+  function_name = "lambda"
+  role          = aws_iam_role.role.arn
+  handler       = "lambda.handler"
+  runtime       = "nodejs12.x"
+}
