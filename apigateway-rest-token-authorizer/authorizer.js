a4d703b565966460458d8a4ed12d6912f2ef26ae
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-04-25 22:29:47 +0100

    fix lambda authorizer event payload

--- apigateway-rest-token-authorizer/authorizer.js
+++ apigateway-rest-token-authorizer/authorizer.js
@@ -1,20 +1,18 @@
 function generatePolicy(methodArn) {
-  return {
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Action: "execute-api:Invoke",
-        Effect: "Allow",
-        Resource: methodArn,
-      },
-    ],
-  };
+    return {
+        Version: "2012-10-17",
+        Statement: [{
+            Action: "execute-api:Invoke",
+            Effect: "Allow",
+            Resource: methodArn,
+        }, ],
+    };
 }
 
 exports.handler = function(event, context, callback) {
-		console.log(event);
-		return {
-				principalId: 'me',
-				policyDocument: generatePolicy(event.methodArn)
+    console.log(event);
+    return {
+        principalId: 'me',
+        policyDocument: generatePolicy(event.methodArn)
     };
 }
