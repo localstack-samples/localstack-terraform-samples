4d2de8518a33a5c930e08ae0d6bd6e07fcf1aabb
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-05-22 11:22:28 +0100

    apigatewayv2-jwt-authorizer

---
+++ apigatewayv2-jwt-authorizer/README.md
@@ -0,0 +1,31 @@
+## Run Terraform
+
+`terraform init; terraform plan; terraform apply --auto-approve`
+
+## Create Cognito users
+
+Using the outputs run the following commands,
+
+```
+ awslocal cognito-idp sign-up \
+      --client-id <user_pool_client_id> \
+      --username "user@domain.com" \
+      --password "Ppassword123!"
+```
+
+then,
+
+```
+awslocal cognito-idp admin-confirm-sign-up \
+      --user-pool-id <user_pool_id> \
+      --username "user@domain.com"
+```
+
+then,
+
+```
+awslocal cognito-idp initiate-auth \
+      --auth-flow USER_PASSWORD_AUTH \
+      --auth-parameters USERNAME="user@domain.com",PASSWORD="password" \
+      --client-id <user_pool_client_id>
+```
---
+++ apigatewayv2-jwt-authorizer/main.tf
@@ -0,0 +1,139 @@
+resource "aws_apigatewayv2_api" "example" {
+  name          = "example-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_authorizer" "user" {
+  api_id           = aws_apigatewayv2_api.example.id
+  authorizer_type  = "JWT"
+  identity_sources = ["$request.header.Authorization"]
+	name             = "user-authorizer"
+
+	jwt_configuration {
+		audience = ["user"]
+    issuer   = "https://${aws_cognito_user_pool.pool.endpoint}"
+  }
+}
+
+resource "aws_apigatewayv2_authorizer" "admin" {
+  api_id           = aws_apigatewayv2_api.example.id
+  authorizer_type  = "JWT"
+  identity_sources = ["$request.header.Authorization"]
+	name             = "admin-authorizer"
+
+	jwt_configuration {
+		audience = ["admin"]
+    issuer   = "https://${aws_cognito_user_pool.pool.endpoint}"
+  }
+}
+
+
+resource "aws_cognito_user_pool" "pool" {
+  name = "user_pool"
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name         = "example_external_api"
+  user_pool_id = aws_cognito_user_pool.pool.id
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_USER_SRP_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
+}
+
+resource "aws_apigatewayv2_integration" "user" {
+  api_id                 = aws_apigatewayv2_api.example.id
+  integration_type       = "AWS_PROXY"
+  payload_format_version = "2.0"
+  description            = "Lambda example"
+  integration_method     = "ANY"
+  integration_uri        = aws_lambda_function.user.invoke_arn
+}
+
+resource "aws_apigatewayv2_integration" "admin" {
+  api_id                 = aws_apigatewayv2_api.example.id
+  integration_type       = "AWS_PROXY"
+  payload_format_version = "2.0"
+  description            = "Lambda example"
+  integration_method     = "ANY"
+  integration_uri        = aws_lambda_function.admin.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "user" {
+  api_id             = aws_apigatewayv2_api.example.id
+  route_key          = "ANY /users/user"
+  authorization_type = "CUSTOM"
+  authorizer_id      = aws_apigatewayv2_authorizer.user.id
+  target             = "integrations/${aws_apigatewayv2_integration.user.id}"
+
+}
+resource "aws_apigatewayv2_route" "admin" {
+  api_id             = aws_apigatewayv2_api.example.id
+  route_key          = "ANY /users/admin"
+  authorization_type = "CUSTOM"
+  authorizer_id      = aws_apigatewayv2_authorizer.admin.id
+  target             = "integrations/${aws_apigatewayv2_integration.admin.id}"
+}
+
+resource "aws_lambda_function" "admin" {
+  filename      = "admin.zip"
+  function_name = "mylambda"
+  role          = aws_iam_role.role.arn
+  handler       = "admin.handler"
+
+  source_code_hash = filebase64sha256("admin.zip")
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+resource "aws_lambda_function" "user" {
+  filename      = "lambda.zip"
+  function_name = "myadminlambda"
+  role          = aws_iam_role.role.arn
+  handler       = "lambda.handler"
+
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name = "myrole"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+output "user_pool_client_id" {
+  value = aws_cognito_user_pool_client.client.id
+}
+
+output "user_pool_id" {
+  value = aws_cognito_user_pool.pool.id
+}
