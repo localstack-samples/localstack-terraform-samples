8bb7cc56c901b3f6eedc8b7324958e04a56b67a2
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-05-27 10:56:16 +0100

    apigatewayv2-lambda-request-authorizer

--- apigatewayv2-lambda-request-authorizer/lambda-auth.js
+++ apigatewayv2-lambda-request-authorizer/lambda-auth.js
@@ -1,8 +1,44 @@
 exports.handler = async(event) => {
-		return {
-				"isAuthorized": true,
-				"context": {
-						"exampleKey": "exampleValue"
-				}
-		}
-}
+  if (event.headers.authorization == "secretToken") {
+    console.log("allowed");
+    return {
+      "principalId": "abcdef", // The principal user identification associated with the token sent by the client.
+      "policyDocument": {
+        "Version": "2012-10-17",
+        "Statement": [{
+          "Action": "execute-api:Invoke",
+          "Effect": "Allow",
+          "Resource": event.routeArn
+        }]
+      },
+      "context": {
+        "stringKey": "value",
+        "numberKey": 1,
+        "booleanKey": true,
+        "arrayKey": ["value1", "value2"],
+        "mapKey": { "value1": "value2" }
+      }
+    };
+  }
+  else {
+    console.log("denied");
+    return {
+      "principalId": "abcdef", // The principal user identification associated with the token sent by the client.
+      "policyDocument": {
+        "Version": "2012-10-17",
+        "Statement": [{
+          "Action": "execute-api:Invoke",
+          "Effect": "Deny",
+          "Resource": event.routeArn
+        }]
+      },
+      "context": {
+        "stringKey": "value",
+        "numberKey": 1,
+        "booleanKey": true,
+        "arrayKey": ["value1", "value2"],
+        "mapKey": { "value1": "value2" }
+      }
+    };
+  }
+};
--- apigatewayv2-lambda-request-authorizer/run.sh
+++ apigatewayv2-lambda-request-authorizer/run.sh
@@ -3,4 +3,4 @@
 terraform init; terraform plan; terraform apply --auto-approve
 
 httpapi=$(aws --endpoint-url=http://localhost:4566 apigatewayv2 get-apis | jq -r .Items[0].ApiId)
-curl -X POST "$httpapi.execute-api.localhost.localstack.cloud:4566/example/test" -H 'content-type: application/json' -d '{ "greeter": "cesar" }' --cookie "Cookie1=Yes;Cookie2=no"
+curl -vvv -X POST "$httpapi.execute-api.localhost.localstack.cloud:4566/example/test" -H 'Authorization: not-secret-token'
