29336040d20dfa16d5a9ca1c53ecbc40c9ad09ac
calvernaz <cesar.alvernaz@gmail.com>
2022-07-17 15:42:39 +0000

    update example apigateway-custom-domain

--- apigateway-custom-domain/main.tf
+++ apigateway-custom-domain/main.tf
@@ -62,11 +62,11 @@ resource "aws_iam_role" "role" {
 POLICY
 }
 
-resource "aws_route53_zone" "example" {
+resource "aws_route53_zone" "dns_zone" {
   name = "example.com"
 }
 
-resource "aws_api_gateway_domain_name" "example" {
+resource "aws_api_gateway_domain_name" "domain_name" {
   certificate_body          = file("${path.module}/sslcert/server.crt")
   certificate_chain         = file("${path.module}/sslcert/rootCA.key")
   certificate_private_key   = file("${path.module}/sslcert/ssl.key")
@@ -81,13 +81,13 @@ resource "aws_api_gateway_domain_name" "example" {
 # Example DNS record using Route53.
 # Route53 is not specifically required; any DNS host can be used.
 resource "aws_route53_record" "example" {
-  name    = aws_api_gateway_domain_name.example.domain_name
+  name    = aws_api_gateway_domain_name.domain_name.domain_name
   type    = "A"
-  zone_id = aws_route53_zone.example.id
+  zone_id = aws_route53_zone.dns_zone.id
 
   alias {
     evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.example.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.example.regional_zone_id
+    name                   = aws_api_gateway_domain_name.domain_name.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.domain_name.regional_zone_id
   }
 }
--- apigateway-custom-domain/run.sh
+++ apigateway-custom-domain/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-terraform init; terraform plan; terraform apply --auto-approve
+rm terraform.tfstate || true
+
+terraform init; terraform plan; TF_LOG=debug terraform apply --auto-approve
 
 restapi=$(aws apigateway --endpoint-url=http://localhost:4566 get-rest-apis | jq -r .items[0].id)
 curl $restapi.execute-api.localhost.localstack.cloud:4566/local/test -H "Host: example.com"
