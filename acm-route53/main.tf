33fa50f136c2747afd59acf9931e16a768f55e0d
Harsh Mishra <erbeusgriffincasper@gmail.com>
2022-11-14 23:57:47 +0530

    add sample for acm certificate (#3)

---
+++ acm-route53/main.tf
@@ -0,0 +1,30 @@
+variable "root_domain_name" {
+  type    = string
+  default = "helloworld.info"
+}
+
+resource "aws_route53_zone" "hello_world_zone" {
+  name = var.root_domain_name
+}
+
+resource "aws_acm_certificate" "hello_certificate" {
+  domain_name       = var.root_domain_name
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "hello_cert_dns" {
+  allow_overwrite = true
+  name            = tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_name
+  records         = [tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_value]
+  type            = tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_type
+  zone_id         = aws_route53_zone.hello_world_zone.zone_id
+  ttl             = 60
+}
+
+resource "aws_acm_certificate_validation" "hello_cert_validate" {
+  certificate_arn         = aws_acm_certificate.hello_certificate.arn
+  validation_record_fqdns = [aws_route53_record.hello_cert_dns.fqdn]
+}
