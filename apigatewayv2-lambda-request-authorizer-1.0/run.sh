b4528b9e83e8fec900852c9102cb817b34c53955
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-11-24 07:02:46 +0000

    update lambda auth policies

--- apigatewayv2-lambda-request-authorizer-1.0/run.sh
+++ apigatewayv2-lambda-request-authorizer-1.0/run.sh
@@ -2,9 +2,9 @@
 
 rm -rf terraform.tfstate* || true
 
-terraform init; terraform plan; terraform apply --auto-approve
+tflocal init; tflocal plan; tflocal apply --auto-approve
 
 httpapi=$(aws --endpoint-url=http://localhost:4566 apigatewayv2 get-apis | jq -r .Items[0].ApiId)
-#curl -vvv -X POST "$httpapi.execute-api.localhost.localstack.cloud:4566/example/test" -H 'Authorization: not-secret-token'
 
+#curl -vvv -X POST "$httpapi.execute-api.localhost.localstack.cloud:4566/example/test" -H 'Authorization: not-secret-token'
 curl -vvv -X POST "$httpapi.execute-api.localhost.localstack.cloud:4566/example/test" -H 'Authorization: secretToken'
