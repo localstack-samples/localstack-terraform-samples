27763d80c0c70b2559da74cf455dc46bb6e9b7b1
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-11-27 18:04:05 +0000

    apigateway-state-machine

--- apigateway-state-machine/main.tf
+++ apigateway-state-machine/main.tf
@@ -1,35 +1,115 @@
-data "aws_region" "current" {}
-
-resource "aws_api_gateway_rest_api" "api" {
-  name = "example-sfn-api"
+resource "random_pet" "random" {
+  length = 2
 }
 
-resource "aws_api_gateway_resource" "example" {
+data "aws_region" "current" {}
+
+resource "aws_iam_role" "apigateway" {
+  name               = "rest-apigw-sfn"
+  assume_role_policy = data.aws_iam_policy_document.apigw_assume.json
+}
+
+data "aws_iam_policy_document" "apigw_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy_attachment" "policy_invoke_sfn" {
+  name       = random_pet.random.id
+  roles      = [aws_iam_role.apigateway.name]
+  policy_arn = "arn:aws:iam::aws:policy/AWSStepFunctionsFullAccess"
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = random_pet.random.id
+}
+
+resource "aws_api_gateway_resource" "resource" {
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "test"
   rest_api_id = aws_api_gateway_rest_api.api.id
+  path_part   = "test"
 }
 
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.example.id
+  resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "state_machine_lmbd" {
+resource "aws_api_gateway_integration" "integration" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.example.id
+  resource_id = aws_api_gateway_resource.resource.id
+  credentials = aws_iam_role.apigateway.arn
   http_method = aws_api_gateway_method.method.http_method
   type        = "AWS"
   uri         = "arn:aws:apigateway:${data.aws_region.current.name}:states:action/StartExecution"
 
   integration_http_method = "POST"
-  passthrough_behavior    = "WHEN_NO_MATCH"
-  content_handling        = "CONVERT_TO_TEXT"
-  request_parameters = {
-    StateMachineArn = aws_sfn_state_machine.sfn_state_machine.arn
-    Input           = "$request.body.Input",
+  request_templates       = {
+    "application/json" = <<EOF
+{
+    "input": "$util.escapeJavaScript($input.json('$'))",
+    "stateMachineArn": "${aws_sfn_state_machine.sfn_state_machine.arn}"
+}
+EOF
+  }
+}
+
+resource "aws_cloudwatch_log_group" "apigateway" {
+  name              = "/aws/apigateway/${aws_api_gateway_rest_api.api.name}"
+  retention_in_days = 3
+}
+
+resource "aws_api_gateway_stage" "example" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "dev"
+
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.apigateway.arn
+    format          = jsonencode({
+      "requestId" : "$context.requestId",
+      "ip" : "$context.identity.sourceIp",
+      "caller" : "$context.identity.caller",
+      "user" : "$context.identity.user",
+      "requestTime" : "$context.requestTime",
+      "httpMethod" : "$context.httpMethod",
+      "resourcePath" : "$context.resourcePath",
+      "status" : "$context.status",
+      "protocol" : "$context.protocol",
+      "responseLength" : "$context.responseLength"
+    })
+  }
+
+  depends_on = [aws_api_gateway_method.method]
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "response_200" {
+  http_method = aws_api_gateway_method.method.http_method
+  resource_id = aws_api_gateway_resource.resource.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  status_code = aws_api_gateway_method_response.response_200.status_code
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -44,12 +124,12 @@ data "aws_iam_policy_document" "sfn_assume" {
 }
 
 resource "aws_iam_role" "sfn" {
-  name               = "api-sfn"
+  name               = "rest-api-sfn"
   assume_role_policy = data.aws_iam_policy_document.sfn_assume.json
 }
 
 resource "aws_sfn_state_machine" "sfn_state_machine" {
-  name     = "my-state-machine"
+  name     = random_pet.random.id
   role_arn = aws_iam_role.sfn.arn
 
   definition = <<EOF
@@ -67,39 +147,61 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
 EOF
 }
 
+resource "aws_iam_policy" "policy_invoke_lambda" {
+  name = "RestStepFunctionLambdaFunctionInvocationPolicy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "lambda:InvokeFunction",
+                "lambda:InvokeAsync"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "iam_for_sfn_attach_policy_invoke_lambda" {
+  role       = aws_iam_role.sfn.name
+  policy_arn = aws_iam_policy.policy_invoke_lambda.arn
+}
+
+#
+# Lambda
+#
 resource "aws_lambda_function" "lambda" {
   filename      = "lambda.zip"
-  function_name = "mylambda"
-  role          = aws_iam_role.role.arn
+  function_name = random_pet.random.id
+  role          = aws_iam_role.lambda_role.arn
   handler       = "lambda.handler"
 
   source_code_hash = filebase64sha256("lambda.zip")
 
-  runtime = "nodejs12.x"
+  runtime = "nodejs14.x"
+}
 
-  environment {
-    variables = {
-      foo = "bar"
+resource "aws_iam_role" "lambda_role" {
+  name               = "rest-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
+}
+
+data "aws_iam_policy_document" "lambda_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
   }
 }
 
-resource "aws_iam_role" "role" {
-  name = "myrole"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-POLICY
+resource "aws_iam_role_policy_attachment" "lambda_policy_auth" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
