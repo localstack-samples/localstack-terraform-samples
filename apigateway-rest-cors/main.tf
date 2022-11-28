f0509578c3f524fb9eba80343bcf052aa56f9d29
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-11-28 22:01:10 +0000

    apigateway-rest-cors

--- apigateway-rest-cors/main.tf
+++ apigateway-rest-cors/main.tf
@@ -1,18 +1,36 @@
+resource "aws_iam_role" "apigateway" {
+  name               = "rest-apigw-sfn"
+  assume_role_policy = data.aws_iam_policy_document.apigw_assume.json
+}
+
+data "aws_iam_policy_document" "apigw_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_api_gateway_rest_api" "cors_api" {
   name        = "cors-demo"
   description = "An API for demonstrating CORS-enabled methods."
 }
+
 resource "aws_api_gateway_resource" "cors_resource" {
   path_part   = "test"
   parent_id   = aws_api_gateway_rest_api.cors_api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.cors_api.id
 }
+
 resource "aws_api_gateway_method" "options_method" {
   rest_api_id   = aws_api_gateway_rest_api.cors_api.id
   resource_id   = aws_api_gateway_resource.cors_resource.id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
+
 resource "aws_api_gateway_method_response" "options_200" {
   rest_api_id = aws_api_gateway_rest_api.cors_api.id
   resource_id = aws_api_gateway_resource.cors_resource.id
@@ -30,13 +48,19 @@ resource "aws_api_gateway_method_response" "options_200" {
   }
   depends_on = [aws_api_gateway_method.options_method]
 }
+
 resource "aws_api_gateway_integration" "options_integration" {
   rest_api_id = aws_api_gateway_rest_api.cors_api.id
   resource_id = aws_api_gateway_resource.cors_resource.id
   http_method = aws_api_gateway_method.options_method.http_method
   type        = "MOCK"
   depends_on  = [aws_api_gateway_method.options_method]
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
 }
+
 resource "aws_api_gateway_integration_response" "options_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.cors_api.id
   resource_id = aws_api_gateway_resource.cors_resource.id
@@ -48,4 +72,43 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
   depends_on = [aws_api_gateway_method_response.options_200]
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.cors_api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.cors_api.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_cloudwatch_log_group" "apigateway" {
+  name              = "/aws/apigateway/${aws_api_gateway_rest_api.cors_api.name}"
+  retention_in_days = 3
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.cors_api.id
+  stage_name    = "dev"
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.apigateway.arn
+    format = jsonencode({
+      "requestId" : "$context.requestId",
+      "ip" : "$context.identity.sourceIp",
+      "caller" : "$context.identity.caller",
+      "user" : "$context.identity.user",
+      "requestTime" : "$context.requestTime",
+      "httpMethod" : "$context.httpMethod",
+      "resourcePath" : "$context.resourcePath",
+      "status" : "$context.status",
+      "protocol" : "$context.protocol",
+      "responseLength" : "$context.responseLength"
+    })
+  }
 }
