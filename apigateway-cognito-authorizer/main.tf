4339d140825452ad47cdbd3236ba49bbfb5bfcc8
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-12-14 16:49:14 +0000

    apigateway-cognito-authorizer

--- apigateway-cognito-authorizer/main.tf
+++ apigateway-cognito-authorizer/main.tf
@@ -1,38 +1,33 @@
 data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
-# data "aws_region" "current" {
-#   provider = aws.localstack
-# }
+resource "random_pet" "random" {
+  length = 2
+}
 
-# locals {
-#   account_id = data.aws_caller_identity.current.account_id
-#   region     = data.aws_region.current.name
-#}
-
-
-resource "aws_api_gateway_rest_api" "demo" {
-  name        = "auth-demo"
+resource "aws_api_gateway_rest_api" "api" {
+  name        = random_pet.random.id
   description = "cognito-authorization"
 }
 
-resource "aws_api_gateway_resource" "demo" {
-  rest_api_id = aws_api_gateway_rest_api.demo.id
-  parent_id   = aws_api_gateway_rest_api.demo.root_resource_id
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "demo"
 }
 
-resource "aws_api_gateway_method" "any" {
-  rest_api_id   = aws_api_gateway_rest_api.demo.id
-  resource_id   = aws_api_gateway_resource.demo.id
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "ANY"
   authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.demo.id
+  authorizer_id = aws_api_gateway_authorizer.authorizer.id
 }
 
-resource "aws_api_gateway_authorizer" "demo" {
+resource "aws_api_gateway_authorizer" "authorizer" {
   name            = "demo"
   type            = "COGNITO_USER_POOLS"
-  rest_api_id     = aws_api_gateway_rest_api.demo.id
+  rest_api_id     = aws_api_gateway_rest_api.api.id
   provider_arns   = [aws_cognito_user_pool.pool.arn]
   identity_source = "method.request.header.Authorization"
 }
@@ -42,9 +37,9 @@ resource "aws_api_gateway_authorizer" "demo" {
 #
 
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.demo.id
-  resource_id             = aws_api_gateway_resource.demo.id
-  http_method             = aws_api_gateway_method.any.http_method
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "ANY"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
@@ -58,13 +53,55 @@ resource "aws_lambda_permission" "apigw_lambda" {
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   #  source_arn = "arn:aws:execute-api:${local.region}:${local.account_id}:${aws_api_gateway_rest_api.demo.id}/*/${aws_api_gateway_method.any.http_method}${aws_api_gateway_resource.demo.path}"
-  source_arn = "${aws_api_gateway_rest_api.demo.execution_arn}/*/*/*"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
 }
 
+resource "aws_api_gateway_stage" "example" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "dev"
+
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.apigateway.arn
+    format = jsonencode({
+      "requestId" : "$context.requestId",
+      "ip" : "$context.identity.sourceIp",
+      "caller" : "$context.identity.caller",
+      "user" : "$context.identity.user",
+      "requestTime" : "$context.requestTime",
+      "httpMethod" : "$context.httpMethod",
+      "resourcePath" : "$context.resourcePath",
+      "status" : "$context.status",
+      "protocol" : "$context.protocol",
+      "responseLength" : "$context.responseLength"
+    })
+  }
+
+  depends_on = [aws_api_gateway_method.method]
+}
+
+resource "aws_cloudwatch_log_group" "apigateway" {
+  name              = "/aws/apigateway/${aws_api_gateway_rest_api.api.name}"
+  retention_in_days = 3
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [
+    aws_api_gateway_method.method,
+    aws_api_gateway_integration.integration
+  ]
+}
 
 resource "aws_lambda_function" "lambda" {
   filename      = "lambda.zip"
-  function_name = "mylambda"
+  function_name = random_pet.random.id
   role          = aws_iam_role.role.arn
   handler       = "lambda.handler"
 
@@ -80,7 +117,7 @@ resource "aws_lambda_function" "lambda" {
 }
 
 resource "aws_iam_role" "role" {
-  name = "myrole"
+  name = random_pet.random.id
 
   assume_role_policy = <<POLICY
 {
@@ -104,59 +141,55 @@ POLICY
 # Cognito
 #
 resource "aws_cognito_user_pool" "pool" {
-  name = "user_pool"
+  name = random_pet.random.id
+
+  auto_verified_attributes = ["email"]
+  username_attributes      = ["email"]
+
+  admin_create_user_config {
+    allow_admin_create_user_only = true
+
+  }
 }
 
 resource "aws_cognito_user_pool_client" "client" {
-  name         = "example_external_api"
+  name         = random_pet.random.id
   user_pool_id = aws_cognito_user_pool.pool.id
-  explicit_auth_flows = [
-    "ALLOW_USER_PASSWORD_AUTH",
-    "ALLOW_USER_SRP_AUTH",
-    "ALLOW_REFRESH_TOKEN_AUTH"
+
+  supported_identity_providers = ["COGNITO"]
+
+  allowed_oauth_flows = ["client_credentials"]
+  allowed_oauth_scopes = [
+    "${random_pet.random.id}/notification",
+    "${random_pet.random.id}/cancellation",
   ]
+  allowed_oauth_flows_user_pool_client = true
+  generate_secret        = true
+  refresh_token_validity = 7
 }
 
-
-resource "aws_iam_role" "group_role" {
-  name = "user-group-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "cognito-identity.amazonaws.com"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "cognito-identity.amazonaws.com:aud": "us-east-1:12345678-dead-beef-cafe-123456790ab"
-        },
-        "ForAnyValue:StringLike": {
-          "cognito-identity.amazonaws.com:amr": "authenticated"
-        }
-      }
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_cognito_user_group" "main" {
-  name         = "user-group"
+resource "aws_cognito_resource_server" "resource_server" {
+  name         = random_pet.random.id
+  identifier   = random_pet.random.id
   user_pool_id = aws_cognito_user_pool.pool.id
-  description  = "Managed by Terraform"
-  precedence   = 42
-  role_arn     = aws_iam_role.group_role.arn
+  scope {
+    scope_name        = "notification"
+    scope_description = "for notification API calls"
+  }
+  scope {
+    scope_name        = "cancellation"
+    scope_description = "for cancellation API calls"
+  }
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = random_pet.random.id
+  user_pool_id = aws_cognito_user_pool.pool.id
 }
 
 
 output "rest_api_id" {
-  value = aws_api_gateway_rest_api.demo.id
+  value = aws_api_gateway_rest_api.api.id
 }
 
 output "user_pool_client_id" {
--- apigateway-cognito-authorizer/run.sh
+++ apigateway-cognito-authorizer/run.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
-
-terraform init; terraform plan;
-
-#export TF_LOG=debug
-terraform apply --auto-approve
+tflocal init; tflocal plan; tflocal apply --auto-approve
 
 restapi=$(aws apigateway --endpoint-url=http://localhost:4566 get-rest-apis | jq -r .items[0].id)
 curl $restapi.execute-api.localhost.localstack.cloud:4566/local/demo -H "Authorization: Bearer ey"
+
+# aws cognito-idp describe-user-pool-client --user-pool-id <pool-id> --client-id <client-id>
+#curl -X POST \                                                                                                        0 (18.486s) < 16:30:32
+#      https://legal-lacewing.auth.eu-west-1.amazoncognito.com/oauth2/token \
+#      -H 'authorization: Basic ***c2VndGc3dHRqbjVhZjFmMzU5aDM0N***' \
+#      -H 'content-type: application/x-www-form-urlencoded' \
+#      -d 'grant_type=client_credentials&scope=legal-lacewing/cancellation'
