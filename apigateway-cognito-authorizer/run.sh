34812aeb1f8a97a90833f4fc3be28991f74823d7
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-12-14 21:12:58 +0000

    apigateway-cognito-authorizer

--- apigateway-cognito-authorizer/run.sh
+++ apigateway-cognito-authorizer/run.sh
@@ -7,10 +7,10 @@ pool_id=$(tflocal output -json | jq -r .user_pool_id.value)
 secret=$(aws cognito-idp --endpoint-url=http://localhost:4566 describe-user-pool-client --user-pool-id $pool_id --client-id $client_id | jq -r .UserPoolClient.ClientSecret)
 client_name=$(aws cognito-idp --endpoint-url=http://localhost:4566 describe-user-pool-client --user-pool-id $pool_id --client-id $client_id | jq -r .UserPoolClient.ClientName)
 
-access_token=$(curl -X POST http://localhost:4566/oauth2/token \
-                  -H 'authorization: Basic $secret' \
+access_token=$(curl http://localhost:4566/oauth2/token \
+									-u "$client_id:$secret" \
                   -H 'content-type: application/x-www-form-urlencoded' \
-                  -d 'grant_type=client_credentials&scope=$client_name/cancellation' | jq -r .access_token)
+                  -d "grant_type=client_credentials&scope=$client_name/cancellation" | jq -r .access_token)
 
 restapi=$(aws apigateway --endpoint-url=http://localhost:4566 get-rest-apis | jq -r .items[0].id)
 curl $restapi.execute-api.localhost.localstack.cloud:4566/local/demo -H "Authorization: Bearer $access_token"
