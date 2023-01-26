9bbcf18471eb377b52109bbdc8d93e86a9e80cbc
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2023-01-26 08:53:10 +0000

    update apigateway-step-functions-integration]

--- apigateway-step-functions-integration/run.sh
+++ apigateway-step-functions-integration/run.sh
@@ -5,4 +5,4 @@ rm terraform.tfstate* || true
 tflocal init; tflocal plan; tflocal apply --auto-approve
 
 restapi=$(aws --endpoint-url=http://localhost:4566 apigateway  get-rest-apis | jq -r .items[0].id)
-curl "$restapi.execute-api.localhost.localstack.cloud:4566/local/test" -H 'content-type: application/json' -d '{ "input": "{}", "name": "MyExecution", "stateMachineArn": "arn:aws:states:eu-west-1:000000000000:stateMachine:my-state-machine"}'
+curl "$restapi.execute-api.localhost.localstack.cloud:4566/local/test" -H 'content-type: application/json' -d '{ "input": "{}", "name": "MyExecution"}'
