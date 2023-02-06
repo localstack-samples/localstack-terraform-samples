78e516c8ec52d249031c679435209f312e9a7c48
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2023-02-06 17:23:54 +0000

    apigatewayv2-lambda-proxy-integration

--- apigatewayv2-lambda-proxy-integration/run.sh
+++ apigatewayv2-lambda-proxy-integration/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
+rm terraform.tfstate* || true
+
 tflocal init; tflocal plan; tflocal apply --auto-approve
 
 http_api=$(aws --endpoint-url=http://localhost:4566 apigatewayv2 get-apis | jq -r .Items[0].ApiId)
 
 curl -X POST "$http_api.execute-api.localhost.localstack.cloud:4566/package/123/payloads"
 
-curl -X POST "$http_api.execute-api.localhost.localstack.cloud:4566/package
+curl -X POST "$http_api.execute-api.localhost.localstack.cloud:4566/package"
