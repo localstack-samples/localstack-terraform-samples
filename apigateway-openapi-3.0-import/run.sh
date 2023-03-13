5b699137a392d4e58a67fbd0fd0a2bc49bdbe5b2
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2023-03-13 21:22:04 +0000

    apigateway-openapi-3.0-import

--- apigateway-openapi-3.0-import/run.sh
+++ apigateway-openapi-3.0-import/run.sh
@@ -5,6 +5,6 @@ set -x
 tflocal init; tflocal plan; tflocal apply --auto-approve
 
 restapi=$(aws apigateway --endpoint-url=http://localhost:4566 get-rest-apis | jq -r .items[0].id)
-curl -vvv "$restapi.execute-api.localhost.localstack.cloud:4566/dev/api/v1/user/techops+proviosion@gmail.com"
+curl -vvv "$restapi.execute-api.localhost.localstack.cloud:4566/dev/api/v1/user/techops+proviosion@gmail.com" -H "X-Authorization: allow"
 
-curl -vvv "$restapi.execute-api.localhost.localstack.cloud:4566/dev/api/v1/user/techops%2Bproviosion@gmail.com"
+curl -vvv "$restapi.execute-api.localhost.localstack.cloud:4566/dev/api/v1/user/techops%2Bproviosion@gmail.com" -H "X-Authorization: deny"
