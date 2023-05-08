af922f98b7e4a51378177339b33c4e0af00cbf87
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2023-05-08 22:18:33 +0100

    apigateway-rest-cors

--- apigateway-rest-cors/run.sh
+++ apigateway-rest-cors/run.sh
@@ -3,4 +3,4 @@
 tflocal init; tflocal plan; tflocal apply --auto-approve
 
 restapi=$(aws --endpoint-url=http://localhost:4566 apigateway  get-rest-apis | jq -r .items[0].id)
-curl -X OPTIONS -vvv "http://localhost:4566/restapis/$restapi/local/_user_request_/test"
+curl -X OPTIONS -vvv "http://localhost:4566/restapis/$restapi/local/_user_request_/test" -H "Access-Control-Request-Headers: Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token"
