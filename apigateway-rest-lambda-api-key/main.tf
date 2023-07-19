8d3d5682c9544db1695e4fd689774d9571463927
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2023-07-19 19:43:53 +0000

    apigateway-rest-lambda-api-key

--- apigateway-rest-lambda-api-key/main.tf
+++ apigateway-rest-lambda-api-key/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt or via a backend.hcl file. See
+  # https://www.terraform.io/docs/backends/config.html#partial-configuration
+  #  backend "s3" {}
+
+  # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
+  # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
+  required_version = "= 1.5.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.60.0, <= 5.8.0"
+    }
+  }
+}
+
 resource "aws_api_gateway_rest_api" "api" {
   name           = "rest"
   description    = "api-key-auth"
--- apigateway-rest-lambda-api-key/run.sh
+++ apigateway-rest-lambda-api-key/run.sh
@@ -3,7 +3,9 @@
 export TF_LOG=trace
 rm terraform.tfstate* || true
 
-terraform init; terraform plan; terraform apply --auto-approve
+tflocal init; tflocal plan; tflocal apply --auto-approve
 
 restapi=$(aws apigateway --endpoint-url=http://localhost:4566 get-rest-apis | jq -r .items[0].id)
-curl -X POST  $restapi.execute-api.localhost.localstack.cloud:4566/local/models -d '{"foo":"bar"}'
+apikeyid=$(aws apigateway --endpoint-url=http://localhost:4566  get-api-keys | jq .items[].id)
+apikeyvalue=$(aws apigateway --endpoint-url=http://localhost:4566  get-api-key --api-key ${apikeyid} --include-value --query "value" --output text)
+curl -X POST  $restapi.execute-api.localhost.localstack.cloud:4566/dev/auth?apiKey=${apikeyvalue}
