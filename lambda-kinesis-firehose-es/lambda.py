54b9b6609ebb2b8942b5e3e75a4bdbd78a40063d
Joel Scheuner <joel.scheuner.dev@gmail.com>
2023-11-09 13:19:20 +0100

    Migrate localstack networking configurations (#9)

--- lambda-kinesis-firehose-es/lambda.py
+++ lambda-kinesis-firehose-es/lambda.py
@@ -9,10 +9,10 @@ logger = logging.getLogger(__name__)
 def handler(event, context):
     try:
         endpoint_url = None
-        # boto instrumentation is a pro feature (https://docs.localstack.cloud/tools/local-endpoint-injection/)
+        # boto instrumentation is a pro feature (https://docs.localstack.cloud/user-guide/tools/transparent-endpoint-injection/)
         # we'll do this manually here
-        if os.environ.get("LOCALSTACK_HOSTNAME"):
-            endpoint_url = "http://{}:{}".format(os.environ["LOCALSTACK_HOSTNAME"], os.environ["EDGE_PORT"])
+        if os.environ.get("AWS_ENDPOINT_URL"):
+            endpoint_url = os.environ["AWS_ENDPOINT_URL"]
             
         kinesis = boto3.client(
             "kinesis", endpoint_url=endpoint_url, region_name=os.environ["AWS_REGION"], verify=False
