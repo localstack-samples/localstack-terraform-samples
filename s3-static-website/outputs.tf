0954589c2527b49841d4a1ac2f6894f274d4c20c
MarcelStranak <97876429+MarcelStranak@users.noreply.github.com>
2024-04-23 16:24:45 +0200

    fix the variable names

--- s3-static-website/outputs.tf
+++ s3-static-website/outputs.tf
@@ -1,11 +1,11 @@
 # Output variable definitions
 
-output "Bucket_Arn" {
+output "bucket_arn" {
   description = "ARN of the bucket"
   value       = aws_s3_bucket.s3_bucket.arn
 }
 
-output "Bucket_Name" {
+output "bucket_name" {
   description = "Name (id) of the bucket"
   value       = aws_s3_bucket.s3_bucket.id
 }
