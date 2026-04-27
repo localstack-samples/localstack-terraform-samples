5f31ba9433968be91c4506538ea3f3324699f474
Harsh Mishra <erbeusgriffincasper@gmail.com>
2026-04-27 19:18:24 +0530

    fixes

--- s3-static-website/outputs.tf
+++ s3-static-website/outputs.tf
@@ -12,9 +12,10 @@ output "name" {
 
 output "domain" {
   description = "Domain name of the bucket"
-  value       = aws_s3_bucket_website_configuration.s3_bucket.website_domain
+  value       = "s3-website.localhost.localstack.cloud:4566"
 }
 
 output "website_endpoint" {
-  value = aws_s3_bucket_website_configuration.s3_bucket.website_endpoint
+  description = "Website endpoint URL"
+  value       = "http://${aws_s3_bucket.s3_bucket.id}.s3-website.localhost.localstack.cloud:4566"
 }
